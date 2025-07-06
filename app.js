b8dba319e9b5b551a25f762a30babb45c15e22b8
Joaquin <joaquincalligaro@gmail.com>
2025-07-05 21:46:32 -0300

    Add dark mode functionality and styles

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const contador = document.getElementById('contador');
 
+const modoBlanco = document.getElementById('modoOscuro');
+
 function sumar() {
   let valor = parseInt(contador.textContent);
   valor++;
@@ -14,4 +16,14 @@ function restar() {
 
 function reiniciar() {
   contador.textContent = 0;
+}
+
+function alternarModo() {
+  document.body.classList.toggle('modo-oscuro');
+
+  if (document.body.classList.contains('modo-oscuro')) {
+    modoBlanco.textContent = 'Modo Blanco';
+  } else {
+    modoBlanco.textContent = 'Modo Oscuro';
+  }
 }
